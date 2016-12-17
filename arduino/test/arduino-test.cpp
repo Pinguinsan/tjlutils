92f1a5d4c13a3edf027d612b310b1550a7b56401
Tyler Lewis <Pinguinsan@gmail.com>
2016-12-17 13:36:02 -0500

    Same as last commit...again - chromebook-kip

--- arduino/test/arduino-test.cpp
+++ arduino/test/arduino-test.cpp
@@ -75,10 +75,10 @@ int main()
     
     std::cout << "Using ArduinoStreamDelay=";
     prettyPrinter->println(arduinoStreamDelay);
-    prettyPrinter->println();
     
     std::cout << "Using ArduinoIOTryCount=";
     prettyPrinter->println(arduinoIOTryCount);
+    prettyPrinter->println();
     
     std::shared_ptr<TStream> serialPort{std::make_shared<UDPDuplex>(clientHostName, clientPortNumber, serverPortNumber, UDPObjectType::UDP_DUPLEX)};
     std::cout << "Creating Arduino object using serial port " << std::quoted(serialPort->portName()) << "..."; 
