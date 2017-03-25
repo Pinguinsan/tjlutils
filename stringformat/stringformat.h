b7214911f1352d721da69699cc38d727114dd33d
Tyler Lewis <Pinguinsan@gmail.com>
2017-03-25 12:58:24 -0400

    Rewrote StringFormat - chromebook-samus

--- stringformat/stringformat.h
+++ stringformat/stringformat.h
@@ -17,6 +17,8 @@
 *    Public license along with tjlutils                                *
 *    If not, see <http://www.gnu.org/licenses/>                        *
 ***********************************************************************/
+#ifndef STRINGFORMAT_H
+#define STRINGFORMAT_H
 
 #include <iostream>
 #include <memory>
@@ -26,6 +28,7 @@
 #include <algorithm>
 #include <utility>
 
+
 template <typename T>
 std::string toStdString(const T &rhs)
 {
@@ -34,20 +37,23 @@ std::string toStdString(const T &rhs)
     return stringStream.str();
 }
 
+/*snprintf style*/
 template<typename ... Args>
 std::string PStringFormat(const char *format, Args ... args)
 {
     ssize_t size = std::snprintf(nullptr, 0, format, args ...) + 1;
-    std::unique_ptr<char[]> stringBuffer{new char[size]}; 
+    std::unique_ptr<char[]> stringBuffer{new char[size]};
     snprintf(stringBuffer.get(), size, format, args ...);
-    return std::string{stringBuffer.get(), stringBuffer.get() + size - 1}; 
+    return std::string{stringBuffer.get(), stringBuffer.get() + size - 1};
 }
 
+/*Base case to break recursion*/
 std::string TStringFormat(const char *formatting)
 {
     return std::string{formatting};
 }
 
+/*C# style String.Format()*/
 template <typename First, typename ... Args>
 std::string TStringFormat(const char *formatting, First first, Args ... args)
 {
@@ -56,33 +62,41 @@ std::string TStringFormat(const char *formatting, First first, Args ... args)
     std::string returnString{formatting};
     std::string copyString{returnString};
 
-    //Found position, length
+    /*[Absolute position, length]*/
     std::vector<std::pair<size_t, size_t>> foundPositions{};
+
+    int smallestValue{-1};
+    size_t smallestValuePosition{0};
+    int smallestValueLength{0};
+
+    /*Iterate through string, finding position and lengths of all matches {x}*/
     while(std::regex_search(copyString, match, targetRegex)) {
         size_t foundPosition{match.position() + (returnString.length() - copyString.length())};
-        foundPositions.emplace_back(foundPosition, match.str().length());
+        int regexMatchNumericValue{0};
+        try {
+            regexMatchNumericValue = std::stoi(returnString.substr(foundPosition + 1, (foundPosition + match.str().length())));
+            if (regexMatchNumericValue < 0) {
+                throw std::runtime_error(TStringFormat("ERROR: In TStringFormat() - Formatted string is invalid (formatting = {0})", formatting));
+            }
+            if ((regexMatchNumericValue < smallestValue) || (smallestValue == -1)) {
+                smallestValue = regexMatchNumericValue;
+                smallestValuePosition = foundPosition;
+                smallestValueLength = match.str().length();
+            }
+        } catch (std::exception e) {
+            std::cout << e.what() << std::endl;
+        }
+
         copyString = match.suffix();
     }
-    if (foundPositions.empty()) {
+    if (smallestValue == -1) {
         throw std::runtime_error(TStringFormat("ERROR: In TStringFormat() - Formatted string is invalid (formatting = {0})", formatting));
     }
-    //<{value}, position>
-    std::vector<std::pair<int, size_t>> foundValues{};
-    try {
-        for (const auto &it : foundPositions) {
-            //Find value in curly brackets, and convert to integer (
-            foundValues.emplace_back(std::stoi(returnString.substr(it.first + 1, it.second - 2)), it.first);
-        }
-    } catch (std::exception &e) {
-        throw std::runtime_error(TStringFormat("ERROR: In TStringFormat() - Formatted string is invalid (formatting = {0})", formatting));
-    }
-    //Sort them
-    std::sort(foundValues.begin(), foundValues.end(), [](const std::pair<int, size_t> &lhs, const std::pair<int, size_t> &rhs) { return lhs.first < rhs.first; });
-    size_t sizeOfToken{toStdString(foundValues.at(0).first).length() + 2};
-    returnString = returnString.substr(0, foundValues.at(0).second)
+    size_t sizeOfToken{toStdString(smallestValue).length() + 2};
+    returnString = returnString.substr(0, smallestValuePosition)
                    + toStdString(first)
-                   + returnString.substr(foundValues.at(0).second + sizeOfToken);
+                   + returnString.substr(smallestValuePosition + sizeOfToken);
     return TStringFormat(returnString.c_str(), args...);
 }
 
-
+#endif //STRINGFORMAT_H
