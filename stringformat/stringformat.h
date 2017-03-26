2ac723be117896be9e181dfa2a9e3e218168c0e2
Tyler Lewis <Pinguinsan@gmail.com>
2017-03-26 16:15:51 -0400

    Fixed TStringFormat for strings that multiple of the same brace tokens - chromebook-kip

--- stringformat/stringformat.h
+++ stringformat/stringformat.h
@@ -27,6 +27,7 @@
 #include <regex>
 #include <algorithm>
 #include <utility>
+#include <tuple>
 
 
 template <typename T>
@@ -39,7 +40,7 @@ std::string toStdString(const T &rhs)
 
 /*snprintf style*/
 template<typename ... Args>
-std::string PStringFormat(const char *format, Args ... args)
+std::string PStringFormat(const char *format, Args& ... args)
 {
     ssize_t size = std::snprintf(nullptr, 0, format, args ...) + 1;
     std::unique_ptr<char[]> stringBuffer{new char[size]};
@@ -55,48 +56,93 @@ std::string TStringFormat(const char *formatting)
 
 /*C# style String.Format()*/
 template <typename First, typename ... Args>
-std::string TStringFormat(const char *formatting, First first, Args ... args)
+std::string TStringFormat(const char *formatting, First& first, Args& ... args)
 {
+    /* Match exactly one opening brace, one or more numeric digit,
+     * then exactly one closing brace, identifying a token */
     static const std::regex targetRegex{"\\{[0-9]+\\}"};
     std::smatch match;
+    
+    /* Copy the formatting string to a std::string, to
+     * make for easier processing, which will eventually
+     * be used (the .c_str() method) to pass the remainder
+     * of the formatting recursively */
     std::string returnString{formatting};
-    std::string copyString{returnString};
+
+    /* Copy the formatting string to another std::string, which
+     * will get modified in the regex matching loop, to remove the
+     * current match from the string and find the next match */
+    std::string copyString{formatting};
 
     /*[Absolute position, length]*/
     std::vector<std::pair<size_t, size_t>> foundPositions{};
 
-    int smallestValue{-1};
-    size_t smallestValuePosition{0};
-    int smallestValueLength{0};
+    std::vector<std::tuple<int, size_t, size_t>> smallestValueInformation{std::make_tuple(-1, 0, 0)};
 
     /*Iterate through string, finding position and lengths of all matches {x}*/
     while(std::regex_search(copyString, match, targetRegex)) {
+        /*Get the absolute position of the match in the original return string*/
         size_t foundPosition{match.position() + (returnString.length() - copyString.length())};
+        //size_t foundPosition{match.position()};
         int regexMatchNumericValue{0};
         try {
             regexMatchNumericValue = std::stoi(returnString.substr(foundPosition + 1, (foundPosition + match.str().length())));
             if (regexMatchNumericValue < 0) {
                 throw std::runtime_error(TStringFormat("ERROR: In TStringFormat() - Formatted string is invalid (formatting = {0})", formatting));
             }
-            if ((regexMatchNumericValue < smallestValue) || (smallestValue == -1)) {
-                smallestValue = regexMatchNumericValue;
-                smallestValuePosition = foundPosition;
-                smallestValueLength = match.str().length();
+            /* If the numeric value in the curly brace token is smaller than
+             * the current smallest (or if the smallest value has not yet been set,
+             * ie it is the first match), set the corresponding smallestX variables */
+            int smallestValue{std::get<0>(smallestValueInformation.at(0))};
+            if ((smallestValue == -1) || (regexMatchNumericValue < smallestValue)) {
+                smallestValueInformation.clear();
+                smallestValueInformation.push_back(std::make_tuple(regexMatchNumericValue,
+                                                                   foundPosition,
+                                                                   match.str().length()));
+            } else if (regexMatchNumericValue == smallestValue) {
+                smallestValueInformation.push_back(std::make_tuple(regexMatchNumericValue,
+                                                                   foundPosition,
+                                                                   match.str().length()));
             }
         } catch (std::exception e) {
+            //TODO: Throw instead of just output exception */
             std::cout << e.what() << std::endl;
         }
-
+        
+        
+        //copyString.replace(foundPosition, match.str().length(), std::string{match.str().length(), '-'});
         copyString = match.suffix();
     }
+    int smallestValue{std::get<0>(smallestValueInformation.at(0))};
     if (smallestValue == -1) {
         throw std::runtime_error(TStringFormat("ERROR: In TStringFormat() - Formatted string is invalid (formatting = {0})", formatting));
     }
-    size_t sizeOfToken{toStdString(smallestValue).length() + 2};
-    returnString = returnString.substr(0, smallestValuePosition)
-                   + toStdString(first)
-                   + returnString.substr(smallestValuePosition + sizeOfToken);
+    /* Set the returnString to be up to the brace token, then the string
+     * representation of current argument in line (first), then the remainder
+     * of the format string, effectively removing the token and replacing it
+     * with the requested item in the final string, then pass it off recursively */
+    
+    std::string firstString{toStdString(first)};
+    int index{0};
+    for (auto &it : smallestValueInformation) {
+        size_t smallestValueLength{std::get<2>(it)};
+
+
+        /*Since the original string will be modified, the adjusted position must be 
+          calculated for any repeated brace tokens, kept track of by index.
+          The length of string representation of first mutiplied by which the iterationn count
+          is added, and the length of the brace token multiplied by the iteration count is
+          subtracted, resulting in the correct starting position of the current brace token */
+        size_t lengthOfTokenBracesRemoved{index * smallestValueLength};
+        size_t lengthOfStringAdded{index * firstString.length()};
+        size_t smallestValueAdjustedPosition{std::get<1>(it) + lengthOfStringAdded - lengthOfTokenBracesRemoved};
+        returnString = returnString.substr(0, smallestValueAdjustedPosition)
+                       + firstString
+                       + returnString.substr(smallestValueAdjustedPosition + smallestValueLength);
+        index++;
+    }
     return TStringFormat(returnString.c_str(), args...);
 }
 
 #endif //TJLUTILS_STRINGFORMAT_H
+
