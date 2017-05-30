e013dab28f8bb04819dfb0a4fd809ec90bbd1761
Tyler Lewis <tlewiscpp@gmail.com>
2017-05-30 19:58:44 -0400

    Auto-Commit - chromebook-kip

--- eventtimer/eventtimer.h
+++ eventtimer/eventtimer.h
@@ -1,10 +1,10 @@
 /***********************************************************************
 *    eventtimer.h:                                                     *
 *    Class EventTimer, for timing events and getting calculated times  *
-*    Copyright (c) 2016 Tyler Lewis                                    *
+*    Copyright (c) 2017 Tyler Lewis                                    *
 ************************************************************************
 *    This is a header file for tjlutils:                               *
-*    https://github.com/tlewiscpp/tjlutils                            *
+*    https://github.com/Pinguinsan/tjlutils                            *
 *    This file may be distributed with the entire tjlutils library,    *
 *    but may also be distributed as a standalone file                  *
 *    The source code is released under the GNU LGPL                    *
@@ -28,9 +28,17 @@
 #include <string>
 #include <thread>
 #include <future>
+#include <sstream>
 #include <list>
 #include <functional>
-#include <sstream>
+
+struct TimePoint
+{
+    unsigned long hours;
+    unsigned long minutes;
+    unsigned long seconds;
+    unsigned long milliseconds;
+};
 
 template <typename ClockType = std::chrono::steady_clock>
 class EventTimer
@@ -41,14 +49,12 @@ public:
     EventTimer<ClockType>() :
         m_startTime{platform_clock_t::now()},
         m_endTime{platform_clock_t::now()},
-        m_cacheStartTime{platform_clock_t::now()},
         m_totalTime{0},
         m_hours{0},
         m_minutes{0},
         m_seconds{0},
         m_milliseconds{0},
-        m_isPaused{false},
-        m_isStopped{false}
+        m_isPaused{false}
     {
 
     }
@@ -56,16 +62,22 @@ public:
     EventTimer<ClockType>(const EventTimer<ClockType> &other) :
         m_startTime{other.m_startTime},
         m_endTime{other.m_endTime},
-        m_cacheStartTime{other.m_cacheStartTime},
         m_totalTime{other.m_totalTime},
         m_hours{other.m_hours},
         m_minutes{other.m_minutes},
         m_seconds{other.m_seconds},
         m_milliseconds{other.m_milliseconds},
-        m_isPaused{other.m_isPaused},
-        m_isStopped{other.m_isStopped}
+        m_isPaused{other.m_isPaused}
     {
 
+    }
+
+    template <typename T>
+    std::string TO_STRING(const T &item)
+    {
+        std::stringstream stringStream{};
+        stringStream << item;
+        return stringStream.str();
     }
 
     void start()
@@ -75,50 +87,36 @@ public:
         this->m_minutes = 0;
         this->m_seconds = 0;
         this->m_milliseconds = 0;
-        this->m_cacheStartTime = platform_clock_t::now();
         this->m_startTime = platform_clock_t::now();
         this->m_endTime = this->m_startTime;
         this->m_isPaused = false;
-        this->m_isStopped = false;
     }
 
-    void restart() 
-    { 
-        return this->start(); 
+    void restart()
+    {
+        return this->start();
     }
 
     void pause()
     {
-        bool stopped{this->m_isStopped};
-        this->m_isStopped = false;
-        this->m_isPaused = false;
-        this->update();
-        this->m_isStopped = stopped;
-        this->m_isPaused = true;
+        this->stop();
     }
 
     void stop()
     {
-        bool paused{this->m_isPaused};
-        this->m_isPaused = false;
-        this->m_isStopped = false;
-        this->update();
-        this->m_isPaused = paused;
-        this->m_isStopped = true;
+        this->m_isPaused = true;
     }
-    
+
     void resume()
     {
-        if (this->m_isStopped) {
-            return this->start();
-        } else {
-            this->m_isPaused = false;
-        }
+        this->m_isPaused = false;
     }
 
     void update()
     {
-        if ((!this->m_isPaused) && (!this->m_isStopped)) {
+        if (this->m_isPaused)  {
+            this->m_startTime = platform_clock_t::now() - this->m_rawTime;
+        } else {
             this->m_endTime = platform_clock_t::now();
             this->m_totalTime = std::chrono::duration_cast<std::chrono::milliseconds>(this->m_endTime-this->m_startTime).count();
             this->m_rawTime = std::chrono::duration_cast<std::chrono::milliseconds>(this->m_endTime - this->m_startTime);
@@ -126,153 +124,116 @@ public:
             this->m_minutes = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR)) / MILLISECONDS_PER_MINUTE;
             this->m_seconds = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR) - (this->m_minutes * MILLISECONDS_PER_MINUTE)) / MILLISECONDS_PER_SECOND;
             this->m_milliseconds = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR) - (this->m_minutes * MILLISECONDS_PER_MINUTE) - (this->m_seconds * MILLISECONDS_PER_SECOND));
-        } else if (this->m_isStopped) {
-            this->m_hours = (this->m_totalTime/MILLISECONDS_PER_HOUR);
-            this->m_minutes = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR)) / MILLISECONDS_PER_MINUTE;
-            this->m_seconds = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR) - (this->m_minutes * MILLISECONDS_PER_MINUTE)) / MILLISECONDS_PER_SECOND;
-            this->m_milliseconds = (this->m_totalTime - (this->m_hours * MILLISECONDS_PER_HOUR) - (this->m_minutes * MILLISECONDS_PER_MINUTE) - (this->m_seconds * MILLISECONDS_PER_SECOND));
-        } else {
-            this->m_startTime = platform_clock_t::now() - this->m_rawTime;
         }
     }
 
     long long int totalMicroseconds()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return static_cast<long long int>(static_cast<double>(this->m_totalTime) * MICROSECONDS_PER_MILLISECOND);
+    }
+
+    TimePoint timePoint()
+    {
+        this->update();
+        TimePoint returnPoint;
+        returnPoint.hours = this->m_hours;
+        returnPoint.minutes = this->m_minutes;
+        returnPoint.seconds = this->m_seconds;
+        returnPoint.milliseconds = this->m_milliseconds;
+        return returnPoint;
     }
 
     long long int totalMilliseconds()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return this->m_totalTime;
     }
 
     long long int totalSeconds()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return static_cast<long long int>(static_cast<double>(this->m_totalTime) / MILLISECONDS_PER_SECOND);
     }
 
     long long int totalMinutes()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return static_cast<long long int>(static_cast<double>(this->m_totalTime) / MILLISECONDS_PER_MINUTE);
     }
 
     long long int totalHours()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return static_cast<long long int>(static_cast<double>(this->m_totalTime) / MILLISECONDS_PER_HOUR);
     }
 
     long long int totalTime()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
+        this->update();
         return this->m_totalTime;
     }
 
     std::string toString(uint8_t millisecondDigits = 3)
     {
-        if (!this->cacheIsValid()) {
-            this->validateCache();
+        if (!this->m_isPaused) {
             this->update();
         }
         std::string returnString{""};
-        if (this->hours() != 0) {
-            returnString = toStdString(this->hours()) + ':';
+        if (this->m_hours != 0) {
+            returnString = TO_STRING(this->m_hours) + ':';
         }
-        returnString += toStdString(this->minutes())
+        returnString += TO_STRING(this->m_minutes)
                         + ':'
-                        + toStdString(this->seconds())
+                        + TO_STRING(this->m_seconds)
                         + '.';
-        long long int millisecond{this->milliseconds()};
+        long long int millisecond{this->m_milliseconds};
         std::string millisecondsString{""};
         if (millisecond < 10) {
-            millisecondsString = "00" + toStdString(millisecond);
+            millisecondsString = "00" + TO_STRING(millisecond);
         } else if (millisecond < 100) {
-            millisecondsString = "0" + toStdString(millisecond);
+            millisecondsString = "0" + TO_STRING(millisecond);
         } else {
-            millisecondsString = toStdString(millisecond);
+            millisecondsString = TO_STRING(millisecond);
         }
         returnString += millisecondsString.substr(0, millisecondDigits);
-        
+
         return returnString;
     }
 
     long long int hours()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
         return this->m_hours;
     }
-    
+
     long long int minutes()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
         return this->m_minutes;
     }
-    
+
     long long int seconds()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
         return this->m_seconds;
     }
-    
+
     long long int milliseconds()
     {
-        if  (!this->cacheIsValid()) {
-            this->validateCache();
-            this->update();
-        }
         return this->m_milliseconds;
     }
-    
+
     inline bool isPaused() const
     {
         return this->m_isPaused;
     }
 
-    inline bool isStopped() const
-    {
-        return this->m_isStopped;
-    }
-    
     inline bool isRunning() const
     {
-        return ((!this->isPaused()) && (!this->isStopped()));
+        return (!this->isPaused());
     }
 
 private:
     std::chrono::time_point<platform_clock_t> m_startTime;
     std::chrono::time_point<platform_clock_t> m_endTime;
-    std::chrono::time_point<platform_clock_t> m_cacheStartTime;
     std::chrono::milliseconds m_rawTime;
     long long int m_totalTime;
     long long int m_hours;
@@ -282,46 +243,33 @@ private:
     bool m_isPaused;
     bool m_isStopped;
 
-    inline bool cacheIsValid()
-    {
-        return (std::chrono::duration_cast<std::chrono::milliseconds>(platform_clock_t::now() - this->m_cacheStartTime).count() < EventTimer::INVALIDATE_CACHE_TIMEOUT);
-    }
+    static const int INVALIDATE_CACHE_TIMEOUT{100};
 
-    inline void validateCache()
-    {
-        this->m_cacheStartTime = platform_clock_t::now();
-    }
     static const long long int constexpr NANOSECONDS_PER_MICROSECOND{1000};
     static const long long int constexpr NANOSECONDS_PER_MILLISECOND{1000000};
     static const long long int constexpr NANOSECONDS_PER_SECOND{1000000000};
     static const long long int constexpr NANOSECONDS_PER_MINUTE{60000000000};
     static const long long int constexpr NANOSECONDS_PER_HOUR{3600000000000};
-    static const long long int constexpr NANOSECONDS_PER_DAY{86400000000000};
-    static const long long int constexpr MICROSECONDS_PER_MILLISECOND{1000};
+    static const long long int constexpr NANOSECONDS_PER_DAY {86400000000000};
+
     static const long long int constexpr MICROSECONDS_PER_SECOND{1000000};
     static const long long int constexpr MICROSECONDS_PER_MINUTE{60000000};
     static const long long int constexpr MICROSECONDS_PER_HOUR{3600000000};
     static const long long int constexpr MICROSECONDS_PER_DAY{86400000000};
     static const long long int constexpr MILLISECONDS_PER_SECOND{1000};
+    static const long long int constexpr MICROSECONDS_PER_MILLISECOND{1000};
     static const long long int constexpr MILLISECONDS_PER_MINUTE{60000};
     static const long long int constexpr MILLISECONDS_PER_HOUR{3600000};
     static const long long int constexpr MILLISECONDS_PER_DAY{86400000};
+
     static const long long int constexpr SECONDS_PER_MINUTE{60};
     static const long long int constexpr SECONDS_PER_HOUR{3600};
+
     static const long long int constexpr MINUTES_PER_HOUR{60};
     static const long long int constexpr MINUTES_PER_DAY{1440};
-    static const long long int constexpr HOURS_PER_DAY{24};
-    static const int INVALIDATE_CACHE_TIMEOUT{100};
 
-    template <typename T> std::string toStdString(T convert) {
-        return (dynamic_cast<std::stringstream&>(std::stringstream{} << convert)).str();
-    }
-    inline std::string toStdString(const std::string &rhs) {
-        return rhs;
-    }
-    inline std::string toStdString(const char *rhs) {
-        return std::string{rhs};
-    }
+    static const long long int constexpr HOURS_PER_DAY{24};
+
 };
 
 typedef EventTimer<std::chrono::steady_clock> SteadyEventTimer;
