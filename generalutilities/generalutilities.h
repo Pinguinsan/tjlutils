8e947fabd614ef6dc3424005a208b382891289e4
Tyler Lewis <Pinguinsan@gmail.com>
2017-07-15 19:43:38 +0000

    Fixed make_unique injection - rpi3-pixel

--- generalutilities/generalutilities.h
+++ generalutilities/generalutilities.h
@@ -704,12 +704,14 @@ namespace GeneralUtilities
 
 #if (__cplusplus < 201402L)
 #if ((defined(_MSC_VER) && (_MSC_VER < 1700)) || (!defined(_MSC_VER)) || defined(__ANDROID__))
+    /*
     namespace std {
         template<typename T, typename ...Args>
         unique_ptr<T> make_unique(Args &&...args) {
             return unique_ptr<T>(new T(forward<Args>(args)...));
         }
     }
+    */
 #endif
 #endif
 
